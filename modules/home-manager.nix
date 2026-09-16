c849c66403205eaf12f2b3dc6a0fa1b97faadab1
Christopher Gubbin <christopher.gubbin@wavephotonics.com>
2026-09-16 10:49:13 +0000

    allowed for user input passthrough

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,11 +1,13 @@
-{inputs, ...}: {
+{inputs, ...}: let
+  starterInputs = inputs;
+in {
   flake-file.inputs.home-manager = {
     url = "https://flakehub.com/f/nix-community/home-manager/0.1";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  flake.modules.nixos.home-manager = {
-    imports = [inputs.home-manager.nixosModules.home-manager];
+  flake.modules.nixos.home-manager = {inputs, ...}: {
+    imports = [starterInputs.home-manager.nixosModules.home-manager];
     home-manager.backupFileExtension = "backup_hm";
     home-manager.extraSpecialArgs = {inherit inputs;};
   };
--- modules/lib/mk-host.nix
+++ modules/lib/mk-host.nix
@@ -1,18 +1,21 @@
-{inputs, ...}: {
+{inputs, ...}: let
+  starterInputs = inputs;
+in {
   flake.lib.mkNixos = {
     self,
+    inputs,
     hostname,
     platform,
   }:
-    inputs.nixpkgs.lib.nixosSystem {
+    starterInputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
-        inputs.self.modules.nixos.system-base
+        starterInputs.self.modules.nixos.system-base
         (
           if platform == "wsl"
-          then inputs.self.modules.nixos.wsl
-          else inputs.self.modules.nixos.native
+          then starterInputs.self.modules.nixos.wsl
+          else starterInputs.self.modules.nixos.native
         )
         self.modules.nixos.${hostname}
       ];
